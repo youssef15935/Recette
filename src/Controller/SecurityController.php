13a75e361aea1ac1a89031f149c1e63746e0b6c4
Youssef Abdeljalil <youssefpaintingtv@gmail.com>
2024-09-28 17:15:44 +0200

    creation of user db , login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Get the last entered username and any authentication error
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'security.logout')]
+    public function logout()
+    {
+    
+        
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Ingredient;
 use Faker\Factory as FakerFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     private \Faker\Generator $faker;
+   
 
     public function __construct(){
         $this->faker = FakerFactory::create('fr_FR');
+        
     }
 
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager ): void
     {
         for( $i = 1; $i < 51; $i++ ) {
             $ingredient = new Ingredient();
@@ -23,7 +27,19 @@ class AppFixtures extends Fixture
             $ingredient->setPrice(mt_rand(0, 100)); 
             $manager->persist($ingredient);
         }
-
+        for ($i=0; $i < 10; $i++) { 
+                    $user = new User();
+                    $user->setfullName($this->faker->name());
+                    $user->setPseudo(mt_rand(0,1));
+                    $user->setEmail($this->faker->email());
+                    $user->setRoles(['ROLE USER']);
+                    //password hashed
+                    $user->setPlainPassword('password');
+                    
+                    $manager ->persist($user);
+                }
         $manager->flush();
-    }
+
+
+}
 }
